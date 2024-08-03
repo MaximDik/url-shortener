f4a2b862e0ba6fc6f09a092618dc731e7542e635
Maxim Dik <maxim22f@yandex.ru>
2024-08-03 12:56:00 +0300

    saveUrl

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -29,6 +29,20 @@ func main() {
 		os.Exit(1)
 	}
 
+	id, err := storage.SaveURL("https://yandex.com", "yandex")
+	if err != nil {
+		log.Error("failed to save url", sl.Err(err))
+		os.Exit(1)
+	}
+
+	log.Info("saved url", slog.Int64("id", id))
+
+	id, err = storage.SaveURL("https://yandex.com", "yandex")
+	if err != nil {
+		log.Error("failed to save url", sl.Err(err))
+		os.Exit(1)
+	}
+
 	_ = storage
 
 	// TODO: init logger:slog
